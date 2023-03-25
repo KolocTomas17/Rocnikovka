ecda62be769f946c9f8e607834882be62696dfb8
KolocTomas17 <t.koloc@outlook.com>
2023-03-25 18:33:11 +0100

    Update

--- src/hero/Hero.js
+++ src/hero/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="hero is-medium">
       <div className="hero-body bbb">
         <div className="container has-text-centered">
-          <h1 className='title' >AutobazarPro</h1>
+          <h1 className="AP" >AutobazarPro</h1>
         </div>
         
       </div>
